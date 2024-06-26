8492a2e95b2e3584d98091308e45d4fefa75abd6
Kenji Yamauchi <yamakenji@gmail.com>
2024-06-26 16:53:37 +0900

    Mod: WordRanker.java Chapter4.3まで完了

--- Chapter4/WordRankerJava/src/WordRanker.java
+++ Chapter4/WordRankerJava/src/WordRanker.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WordRanker {
     public static void main(String[] args) throws Exception {
         List<String> words = Arrays.asList("ada", "Haskell", "scala", "java", "python", "rust");
         List<String> ranking = rankedWords(words);
         System.out.println(ranking);
+        System.out.println(words);
     }
 
     private static int score(String word) {
@@ -19,8 +21,16 @@ public class WordRanker {
         }
     };
 
+    // Chapter4.2
+//    static List<String> rankedWords(List<String> words) {
+//        words.sort(scoreComparator); //ここで既存のリストを変更している！
+//        return words;
+//    }
+
+    // Chapter4.3
     static List<String> rankedWords(List<String> words) {
-        words.sort(scoreComparator); //ここで既存のリストを変更している！
-        return words;
+        return words.stream()
+                .sorted(scoreComparator) // 外部の値を使っている！この関数のスコープ外で定義されている！
+                .collect(Collectors.toList()); //新しいListを返す
     }
 }
