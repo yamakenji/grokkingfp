0dae4f020d88069e99578da3c09c9c66d959e08a
Kenji Yamauchi <yamakenji@gmail.com>
2024-08-16 18:30:16 +0900

    Add: Chapter4.6まで完了

--- Chapter4/WordRankerJava/src/WordRanker.java
+++ Chapter4/WordRankerJava/src/WordRanker.java
@@ -5,9 +5,11 @@ import java.util.stream.Collectors;
 
 public class WordRanker {
     public static void main(String[] args) throws Exception {
-        List<String> words = Arrays.asList("ada", "Haskell", "scala", "java", "python", "rust");
-        List<String> ranking = rankedWords(words);
+        List<String> words = Arrays.asList("ada", "haskell", "scala", "java", "python", "rust");
+        List<String> ranking = rankedWords(scoreComparator, words);
+        List<String> bonusRanking = rankedWords(scoreWithBonusComparator, words);
         System.out.println(ranking);
+        System.out.println(bonusRanking);
         System.out.println(words);
     }
 
@@ -15,9 +17,22 @@ public class WordRanker {
         return word.replace("a", "").length();
     }
 
+    static int scoreWithBonus(String word) {
+        int base = score(word);
+        if (word.contains("c")) return base + 5;
+        else return base;
+    }
+
     static Comparator<String> scoreComparator = new Comparator<String>() {
         public int compare(String w1, String w2) {
             return Integer.compare(score(w2), score(w1));
+        }
+    };
+
+    static Comparator<String> scoreWithBonusComparator = new Comparator<String>() {
+        @Override
+        public int compare(String w1, String w2) {
+            return Integer.compare(scoreWithBonus(w2), scoreWithBonus(w1));
         }
     };
 
@@ -28,9 +43,16 @@ public class WordRanker {
 //    }
 
     // Chapter4.3
-    static List<String> rankedWords(List<String> words) {
+//    static List<String> rankedWords(List<String> words) {
+//        return words.stream()
+//                .sorted(scoreComparator) // 外部の値を使っている！この関数のスコープ外で定義されている！
+//                .collect(Collectors.toList()); //新しいListを返す
+//    }
+
+    // Chapter4.4
+    static List<String> rankedWords(Comparator<String> comparator, List<String> words) {
         return words.stream()
-                .sorted(scoreComparator) // 外部の値を使っている！この関数のスコープ外で定義されている！
-                .collect(Collectors.toList()); //新しいListを返す
+                    .sorted(comparator)
+                    .collect(Collectors.toList());
     }
 }
