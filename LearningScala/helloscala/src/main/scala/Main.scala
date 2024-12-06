f7a6749f199c2d9f776c4d894280aea64f280630
Kenji Yamauchi <yamakenji@gmail.com>
2024-12-06 20:30:02 +0900

    mod: hello, hello.worksheet.sc

--- LearningScala/helloscala/src/main/scala/Main.scala
+++ LearningScala/helloscala/src/main/scala/Main.scala
@@ -2,4 +2,14 @@
   println("Hello world!")
   println(msg)
 
+  val firstName = "Kenji"
+  val lastName = "Yamauchi"
+  println(firstName + " " + lastName)
+
+  val a: Int = 1
+  val b: Int = 1
+  if a == b then println("a == b")
+
 def msg = "I was compiled by Scala 3. :)"
+
+
